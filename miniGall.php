1595f876d7f90d0f70bae016e38bd30fab937b4b
damapic <butakov@damapic.ru>
2015-03-25 14:54:33 +0500

    miniGall
    Добавление описания

--- miniGall.php
+++ miniGall.php
@@ -1,5 +1,16 @@
 <?php
+/*
+name: miniGall
+Мини галлерея.
+Modx Revo.
 
+Часто при перено сайта, небольшие галереи приходится держать в одном TV параметре в виде img/photo1.jpg; img/photo2.gif; img/photo3.png; img/photo4.jpg;
+Сниппет позволяет на основе введенных данных вывести эти изображения в приглядный вид.
+Пример вызова снипета. [[!miniGall? &image=`[[*images]]` ]]
+
+hi@citypeople.ru
+citypeople.ru
+*/
 	$input = $image;
 	//$input = 'img/photo1.jpg; img/photo2.gif; img/photo3.png; img/photo4.jpg;';
 	$clear_div_every = 6;
